6a8e370cad371090a76f2fcce8aa1e96e2d56bb0
Joel Ohman <Joel Ohman>
2016-06-21 12:41:37 -0500

    intialized DataFormate interface, JsonModel, DataParser

---
+++ AdAtTheRightTime/AdAtTheRightTime/Models/JsonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+
+namespace AdAtTheRightTime.Tools
+{
+    public class JsonModel : DataFormat
+    {
+        public string jsonString;
+        public JsonModel()
+        {
+        }
+        public JsonModel(string json)
+        {
+            this.jsonString = json;
+        }
+
+        public void setDataString(string url)
+        {
+            string json = new WebClient().DownloadString(url);
+            this.jsonString = json;
+        }
+    }
+}
