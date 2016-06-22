73631cfa35661d9da06b01a769996b1303966960
Joel Ohman <Joel Ohman>
2016-06-22 14:32:35 -0500

    have saleforce data working while logged in

--- AdAtTheRightTime/AdAtTheRightTime/Controllers/SalesController.cs
+++ AdAtTheRightTime/AdAtTheRightTime/Controllers/SalesController.cs
@@ -1,3 +1,6 @@
+﻿using AdAtTheRightTime.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +10,10 @@ using System.Web.Mvc;
 using Salesforce.Force;
 using WebApplication9.Models;
 
-namespace WebApplication9.Controllers
+namespace AdAtTheRightTime.Controllers
 {
     public class SalesController : Controller
     {
-        // GET: /Orders/
         public async Task<ActionResult> Index()
         {
             var accessToken = Session["AccessToken"].ToString();
@@ -19,9 +21,45 @@ namespace WebApplication9.Controllers
             var instanceUrl = Session["InstanceUrl"].ToString();
 
             var client = new ForceClient(instanceUrl, accessToken, apiVersion);
-            var sales     = await client.QueryAsync<SaleViewModel>("SELECT Name, Total__c FROM Sale__c");
+            var sales = await client.QueryAsync<SaleViewModel>("SELECT Name, Total__c FROM Sale__c");
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
 
-            return View(sales.records);
+                ViewBag.isAdmin = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.isAdmin = "Yes";
+                }
+                return View(sales.records);
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View();
+
+        }
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString().Contains("Admin"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
         }
     }
 }
