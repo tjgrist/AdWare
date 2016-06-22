0d73493957ae4beb604abc2d154ca1f273c8b93b
tjgrst <tjgrst@gmail.com>
2016-06-22 12:05:56 -0500

    A business can only edit their businesses info if it is their own business

--- AdAtTheRightTime/AdAtTheRightTime/Controllers/AccountController.cs
+++ AdAtTheRightTime/AdAtTheRightTime/Controllers/AccountController.cs
@@ -166,7 +166,7 @@ namespace AdAtTheRightTime.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
                     await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
-                    return RedirectToAction("Index", "Users");
+                    return RedirectToAction("Index", "Businesses");
                 }
                 var query = db.Roles.Where(x => !x.Name.Contains("Manager"));
                 ViewBag.Name = new SelectList(query.Where(x => !x.Name.Contains("Super Admin")).ToList(), "Name", "Name");
