4c2942b48a9abee103a695ae80a96766f15fcf3f
Luis Diaz <Luis Diaz>
2016-06-27 11:03:52 -0500

    google trends is working

---
+++ AdAtTheRightTime/AdAtTheRightTime/Models/QueryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdAtTheRightTime.Models
+{
+    public class QueryViewModel
+    {
+        public string Queries { get; set; }
+        public int? BusinessId { get; set; }
+        public string QueryBuiltString { get; set; }
+    }
+}
